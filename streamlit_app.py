9192092f2c0161294a912cc3d0d77fd40ed58ac7
thesuperboy <40296133+thesuperboy@users.noreply.github.com>
2022-12-08 18:30:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 st.title('Summaraiser')
-txt = st.text_area('Text to summarise', '''
+txt = st.text_area('Text to summarise', ''' input your text
     ''')
 classifier = pipeline("summarization")
 output = classifier(txt)
